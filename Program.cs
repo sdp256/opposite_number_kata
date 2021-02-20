e6e64ac623492045738d7edb1beff1794f26e18c
sdp256 <steevnpairy@gmail.com>
2021-02-20 12:34:28 -0600

    initial commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Opposite_number_kata
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            Opposite(number);
+
+
+
+        }
+        public static int Opposite(int number)
+        {
+            
+            int oppositeResult = (number * -1);
+            return oppositeResult;
+        }
+    }
+}
